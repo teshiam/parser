ec7aa487805319b030849bf1d5bed4939509ec6e
James <katchjames@gmail.com>
2021-08-07 19:16:54 +0100

    final final changes

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ app.get('/', (req, res) => {
 
         var xmlinfo = '<?xml version="1.0" encoding="UTF-8"?>'
         var rssinfo = '<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0"><title>TradingView Ideas</title><link>https://www.tradingview.com</link><description>Latest market outlook from popular traders on TradingView.</description><atom:link rel="self" href="https://www.tradingview.com/feed/"></atom:link><language>en</language><lastBuildDate>Sat, 07 Aug 2021 04:32:58 -0500</lastBuildDate>'
-        var end = '</channel></rss>'
+        var end = '</rss>'
 
         feed.items.forEach(item => {
             var feedItem = '<item><title>'+item.title+'</title> <description>'+item['content:encoded'].replace("<![CDATA[", "").replace("]]", "")+'</description><link>'+item.link+'</link> <guid isPermaLink="false">'+item.guid+'</guid><pubDate>'+item.pubDate+'</pubDate><content:encoded>'+item['content:encoded'].replace("<![CDATA[", "").replace("]]", "")+'</content:encoded></item>'
             feedItems.push(feedItem);
         })
 
-        var xml = xmlinfo + rssinfo + channel
+        var xml = xmlinfo + rssinfo
         for (var item in feedItems) {
             xml += feedItems[item];
         }
