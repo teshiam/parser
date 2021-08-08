915c80afde62831e8b18817714ec21d90a168a32
James <katchjames@gmail.com>
2021-08-08 15:04:51 +0100

    final last change

--- index.js
+++ index.js
@@ -17,16 +17,15 @@ app.get('/', (req, res) => {
         let feed = await parser.parseURL('https://www.tradingview.com/feed/?stream=crypto');
         let feedItems = []
 
-        var xmlinfo = '<?xml version="1.0" encoding="UTF-8"?>'
-        var rssinfo = '<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom"><channel xmlns:content="http://purl.org/rss/1.0/modules/content/"><title>TradingView Ideas</title><link>https://www.tradingview.com</link><description>Latest market outlook from popular traders on TradingView.</description><atom:link rel="self" href="https://www.tradingview.com/feed/"></atom:link><language>en</language><lastBuildDate>Sat, 07 Aug 2021 04:32:58 -0500</lastBuildDate>'
+        var xmlinfo = '<?xml version="1.0" encoding="utf-8"?> <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom"> <channel xmlns:content="http://purl.org/rss/1.0/modules/content/"><title>TradingView Ideas</title><link>https://www.tradingview.com</link> <description>Latest market outlook from popular traders on TradingView.</description> <atom:link rel="self" href="https://www.tradingview.com/feed/"></atom:link> <language>en</language> <lastBuildDate>Sun, 08 Aug 2021 08:50:18 -0500</lastBuildDate>'
         var end = '</channel></rss>'
 
         feed.items.forEach(item => {
-            var feedItem = '<item><title>'+item.title+'</title><link>'+item.link+'</link><description>'+item['content:encoded'].replace("<![CDATA[", "").replace("]]", "")+'</description><guid>'+item.guid+'</guid><pubDate>'+item.pubDate+'</pubDate><content:encoded>'+item['content:encoded'].replace("<![CDATA[", "").replace("]]", "")+'</content:encoded></item>'
+            var feedItem = '<item><title>'+item.title+'</title><link>'+item.link+'</link> <description>'+item['content:encoded'].replace("<![CDATA[", "").replace("]]", "")+item.description+'</description> <pubDate>'+item.pubDate+'</pubDate> <guid>'+item.guid+'</guid> <content:encoded>'+item['content:encoded']+'</content:encoded> </item>'
             feedItems.push(feedItem);
         })
 
-        var xml = xmlinfo + rssinfo
+        var xml = xmlinfo
         for (var item in feedItems) {
             xml += feedItems[item];
         }
